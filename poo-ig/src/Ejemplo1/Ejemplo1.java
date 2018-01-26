73e8193593e15846f15f2e1ff8a4a3710d0d1b62
T-102 <T-102@PC253>
2018-01-25 19:46:52 -0600

    aplicancioncita

---
+++ poo-ig/src/Ejemplo1/Ejemplo1.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ejemplo1;
+
+/**
+ *
+ * @author T-102
+ */
+public class Ejemplo1 {
+    public static void main(String[] args) {t yq que bude ir 
+        byte b=2;
+        short s=2;
+        int i=2;
+        long l=2;
+        
+        b=(byte);
+        i=8;
+        s=b;
+        a=(shortil);
+        i=(int);
+        i=(int)i;
+                
+                float f=2.0f;
+                double d=2.0 
+    }       floatf =d+2.
+            florat chche n="n"
+            string x="Hola"
+                    string y="(hora");
+                    string e="43.2";
+                    
+                    float w=integer.parsei+ (y)ifloat.parseflloat(z):
+                    
+    
+            
+}
